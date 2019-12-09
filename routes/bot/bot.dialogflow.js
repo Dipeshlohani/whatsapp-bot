f754e817616efb153f8bf2079413b5ad15ea77bf
Bibek Gaihre <bibekgaihre123@outlook.com>
2019-12-09 23:25:55 +0545

    phone number displayed

--- routes/bot/bot.dialogflow.js
+++ routes/bot/bot.dialogflow.js
@@ -2,36 +2,33 @@ const router = require("express").Router();
 
 const { WebhookClient } = require("dialogflow-fulfillment");
 const { dialogflow } = require("actions-on-google");
-
+const fs = require("fs");
 
 const welcomeService = require("../../modules/welcome/welcome.bot");
 
 const app = dialogflow();
 
 router.get("/", (req, res, next) => {
-    res.json({ message: "Hello from Bot. Please only use POST requests" });
+  res.json({ message: "Hello from Bot. Please only use POST requests" });
 });
 router.post("/", async (req, res, next) => {
+  const agent = new WebhookClient({ request: req, response: res });
+  const welcome = new welcomeService(agent);
+  let intentMap = new Map();
 
-    const agent = new WebhookClient({ request: req, response: res });
-    const welcome = new welcomeService(agent);
-    let intentMap = new Map();
+  console.log("----> " + agent.originalRequest.payload.data.From);
 
-    console.log("----> " + agent.intent);
+  intentMap.set("Default Welcome Intent", () => {
+    return welcome.sendResponse();
+  });
+  //   fs.writeFile(__dirname + "/../../play/dialogflow.json", JSON.stringify(req.body, null, 2));
+  intentMap.set("InfoIntent", () => {
+    return welcome.sendResponse();
+  });
 
-    intentMap.set("Default Welcome Intent", () => {
-        return welcome.sendResponse();
-    });
-    
-    intentMap.set("InfoIntent", () => {
-        return welcome.sendResponse();
-    });
-
-
-    if (agent.intent) {
-        agent.handleRequest(intentMap);
-    }
-
+  if (agent.intent) {
+    agent.handleRequest(intentMap);
+  }
 });
 
 module.exports = router;
