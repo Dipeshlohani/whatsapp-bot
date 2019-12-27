790eb40f135269bb778b56ceaa974b42ddce49ab
Bibek Gaihre <bibekgaihre123@outlook.com>
2019-12-27 13:18:55 +0545

    messenger send api integrated

--- modules/welcome/welcome.bot.js
+++ modules/welcome/welcome.bot.js
@@ -10,44 +10,38 @@ class Welcome {
     // let phone = this.agent.originalRequest.payload.data.From;
     // phone = phone.replace("whatsapp:", "");
 
-    let data = await welcomeController.getUser(
-      this.agent.originalRequest.payload.data.sender.id
-    );
+    let data = await welcomeController.getUser(this.agent.originalRequest.payload.data.sender.id);
     if (data) {
       this.agent.add(`hi ${data.name}. You've already registered. Thank you for being with us.`);
+    } else {
+      this.agent.add(
+        "Hi there I am A.I. Pundit powered by OnMyMobile. I will send daily predictions, do custom puja as per astrology predictions and suggest a seva in nearby temple based on your birth chart. Enter 1 for English, 2 for Telugu."
+      );
     }
-
-    // this.agent.add(
-    //   "Hi there I am A.I. Pundit powered by OnMyMobile. I will send daily predictions, do custom puja as per astrology predictions and suggest a seva in nearby temple based on your birth chart. Enter 1 for English, 2 for Telugu."
-    // );
   }
 
   async saveUser() {
     //Don't delete this
     // let phone = this.agent.originalRequest.payload.data.From;
     // phone = phone.replace("whatsapp:", "");
-    console.log(this.agent.parameters)
+    console.log(this.agent.parameters);
     const fbmsn_id = this.agent.originalRequest.payload.data.sender.id;
-    let {
-      person,
-      gender,
-      date,
-      time,
-      pob,
-      gothra,
-      current_location
-    } = this.agent.parameters;
+    let { person, gender, date, time, pob, gothra, current_location } = this.agent.parameters;
     let { name } = person;
     date = date.split("T")[0];
     time = time.split("T")[1];
     time = time.split("+")[0];
-    let detail = await welcomeController.saveDetails(
-      { name, gender, time, pob, current_location, date, gothra, fbmsn_id }
-    );
-    let { sign, Naksahtra } = await welcomeUtils.getAstroDetails(
-      detail.pob,
-      detail.dob
-    );
+    let detail = await welcomeController.saveDetails({
+      name,
+      gender,
+      time,
+      pob,
+      current_location,
+      date,
+      gothra,
+      fbmsn_id
+    });
+    let { sign, Naksahtra } = await welcomeUtils.getAstroDetails(detail.pob, detail.dob);
     this.agent.add(
       `As per your inputs your rasi: ${sign}, Nakshtra: ${Naksahtra}. You will receive daily astrology prediction from us.`
     );
@@ -75,42 +69,39 @@ class Welcome {
       min: time[1]
     };
 
-     let pob = await welcomeUtils.getGeoLocation(p_o_b);
-      pob = {
-        place: p_o_b,
-        coordinates: {
-          longitude: pob.longitude,
-          latitude: pob.latitude
-        }
-      };
-      let currentLocation = await welcomeUtils.getGeoLocation(current_location);
-      currentLocation = {
-        place: current_location,
-        coordinates: {
-          longitude: currentLocation.longitude,
-          latitude: currentLocation.latitude
-        }
-      };
+    let pob = await welcomeUtils.getGeoLocation(p_o_b);
+    pob = {
+      place: p_o_b,
+      coordinates: {
+        longitude: pob.longitude,
+        latitude: pob.latitude
+      }
+    };
+    let currentLocation = await welcomeUtils.getGeoLocation(current_location);
+    currentLocation = {
+      place: current_location,
+      coordinates: {
+        longitude: currentLocation.longitude,
+        latitude: currentLocation.latitude
+      }
+    };
 
     //DONOT DELETE  this.agent.originalRequest.payload.data.From.replace("whatsapp:", "")
-    let { sign, Naksahtra } = await welcomeUtils.getAstroDetails(
-      pob,
-      dob
-    );
+    let { sign, Naksahtra } = await welcomeUtils.getAstroDetails(pob, dob);
     this.agent.add(
       `As per your inputs your rasi: ${sign}, Nakshtra: ${Naksahtra}. You will receive daily astrology prediction from us.`
     );
-   await welcomeController.saveDetails({
-     name,
-     gender,
-     dob,
-     pob,
-     currentLocation,
-     gothra,
-     fbmsn_id,
-     sign,
-     Naksahtra
-   });
+    await welcomeController.saveDetails({
+      name,
+      gender,
+      dob,
+      pob,
+      currentLocation,
+      gothra,
+      fbmsn_id,
+      sign,
+      Naksahtra
+    });
   }
 
   // async getNakshatra({ pob, dob }) {
