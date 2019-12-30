b009be9150090cda6f40b96ac84951af7eba1b74
Bibek Gaihre <bibekgaihre123@outlook.com>
2019-12-30 16:35:53 +0545

    audio combine:final

--- services/cronHandler.js
+++ services/cronHandler.js
@@ -13,6 +13,7 @@ const NakshatraModel = require("../modules/astrology/nakshatra.model");
 const sampleData = require("../stars.json");
 const access_token = config.get("fbpage_token.access_token");
 const FB = new facebook({ access_token });
+const { exec } = require("child_process");
 
 const fs = require("fs");
 
@@ -115,17 +116,130 @@ const saveAllStarNakshatras = async () => {
 
 // };
 
+const saveAudiofiles = async () => {
+  let users = await UserModel.find({});
+  for (let user of users) {
+    let data = await NakshatraModel.findOne({
+      birth_moon_nakshatra: user.birth_moon_nakshatra
+    });
+    let { sentiment } = data;
+    let command = `ffmpeg -i F:/upwork/whatsapp-bot/assets/audios/intro.wav -i F:/upwork/whatsapp-bot/assets/audios/sankalpam-part2.wav`;
+    switch (sentiment.health) {
+      case "excellent":
+        command += ` -i \t F:/upwork/whatsapp-bot/assets/audios/health_excellent.wav`;
+        break;
+      case "average":
+        command += ` -i \t F:/upwork/whatsapp-bot/assets/audios/health_average.wav`;
+        break;
+      case "negative":
+        command += ` -i \t F:/upwork/whatsapp-bot/assets/audios/health_negative.wav`;
+        break;
+      default:
+        break;
+    }
+    switch (sentiment.emotions) {
+      case "excellent":
+        command += ` -i F:/upwork/whatsapp-bot/assets/audios/emotions_excellent.wav`;
+        break;
+      case "average":
+        command += ` -i F:/upwork/whatsapp-bot/assets/audios/emotions_average.wav`;
+        break;
+      case "negative":
+        command += ` -i F:/upwork/whatsapp-bot/assets/audios/emotions_negative.wav`;
+        break;
+      default:
+        break;
+    }
+    switch (sentiment.profession) {
+      case "excellent":
+        command += ` -i F:/upwork/whatsapp-bot/assets/audios/profession_excellent.wav`;
+        break;
+      case "average":
+        command += ` -i F:/upwork/whatsapp-bot/assets/audios/profession_average.wav`;
+        break;
+      case "negative":
+        command += ` -i F:/upwork/whatsapp-bot/assets/audios/profession_negative.wav`;
+        break;
+      default:
+        break;
+    }
+    switch (sentiment.luck) {
+      case "excellent":
+        command += ` -i \t F:/upwork/whatsapp-bot/assets/audios/luck_excellent.wav`;
+        break;
+      case "average":
+        command += ` -i \t F:/upwork/whatsapp-bot/assets/audios/luck_average.wav`;
+        break;
+      case "negative":
+        command += ` -i \t F:/upwork/whatsapp-bot/assets/audios/luck_negative.wav`;
+        break;
+      default:
+        break;
+    }
+    switch (sentiment.personal_life) {
+      case "excellent":
+        command += ` -i F:/upwork/whatsapp-bot/assets/audios/personal-life-excellent.wav`;
+        break;
+      case "average":
+        command += ` -i F:/upwork/whatsapp-bot/assets/audios/personal-life-average.wav`;
+        break;
+      case "negative":
+        command += ` -i F:/upwork/whatsapp-bot/assets/audios/personal-life-negative.wav`;
+        break;
+      default:
+        break;
+    }
+    switch (sentiment.travel) {
+      case "excellent":
+        command += ` -i F:/upwork/whatsapp-bot/assets/audios/travel-excellent.wav`;
+        break;
+      case "average":
+        command += ` -i F:/upwork/whatsapp-bot/assets/audios/travel-average.wav`;
+        break;
+      case "negative":
+        command += ` -i F:/upwork/whatsapp-bot/assets/audios/travel-negative.wav`;
+        break;
+      default:
+        break;
+    }
+    command += ` -i F:/upwork/whatsapp-bot/assets/audios/prayer-part7.wav -i F:/upwork/whatsapp-bot/assets/audios/asservachanam-part1.wav  -i F:/upwork/whatsapp-bot/assets/audios/asservachanam-part2.wav -i F:/upwork/whatsapp-bot/assets/audios/asservachanam-part3.wav -i F:/upwork/whatsapp-bot/assets/audios/conclusion.wav -filter_complex [0:0][1:0][2:0][3:0][4:0][5:0][6:0][7:0][8:0][9:0][10:0][11:0][12:0]concat=n=13:v=0:a=1[out]  -map [out] F:/prayer_${
+      user.fbmsn_id
+    }_${new Date(data.prediction_date).toLocaleDateString()}.wav`;
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+    });
+
+    //AWS functions here
+    return command;
+  }
+  process.exit();
+};
+
 const sendNotificationToMessenger = async () => {
   let users = await UserModel.find({});
   for (let user of users) {
     let data = await NakshatraModel.findOne({
       birth_moon_nakshatra: user.birth_moon_nakshatra
     });
-    let PSID = user.fbmsn_id;
-    let { prediction } = data;
+    // console.log(new Date().toLocaleDateString());
+    if (new Date(data.prediction_date).toLocaleDateString() == new Date().toLocaleDateString()) {
+      let PSID = user.fbmsn_id;
+      let { prediction } = data;
 
-    let message = `Hi ${user.name} here are your prediction for today. \n 1) Health: ${prediction.health} \n 2) Emotions:${prediction.emotions} \n 3) Profession: ${prediction.profession} \n  4) Luck: ${prediction.luck} \n  5) Personal Life: ${prediction.personal_life} \n 6) Travel: ${prediction.travel} `;
-    await FB.sendMessage({ message, PSID });
+      let message = `Hi ${user.name} here are your prediction for today. \n 1) Health: ${prediction.health} \n 2) Emotions:${prediction.emotions} \n 3) Profession: ${prediction.profession} \n  4) Luck: ${prediction.luck} \n  5) Personal Life: ${prediction.personal_life} \n 6) Travel: ${prediction.travel} `;
+      console.log(message);
+      await FB.sendMessage({ message, PSID });
+      message = `Here is a customized prayer for you. This prayer is from you to Almighty to thank him for the positive predictions, guide you for average predictions and help you to overcome the negative predictions with human will. You can chat the prayer along with the voice.`;
+      await FB.sendMessage({ message, PSID });
+    }
   }
   process.exit();
 };
@@ -156,4 +270,4 @@ const sendNakshatraNotification = async () => {
 // sendNakshatraNotification();
 // saveAllStarNakshatras();
 // sendNotificationToMessenger();
-readfile();
+saveAudiofiles();
--- try.js
+++ try.js
@@ -1,15 +1,124 @@
 const { exec } = require("child_process");
-
-let command = "ffmpeg -i C:/s1.wav -i C:/s2.wav F:/output.wav";
-
-exec(command, (error, stdout, stderr) => {
-  if (error) {
-    console.log(`error: ${error.message}`);
-    return;
-  }
-  if (stderr) {
-    console.log(`stderr: ${stderr}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
+const UserModel = require("./modules/user/user.model");
+const NakshatraModel = require("./modules/astrology/nakshatra.model");
+const mongoose = require("mongoose");
+const config = require("config");
+mongoose.connect(config.get("db.url"), {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
+// "ffmpeg -i F:/upwork/whatsapp-bot/assets/audios/s1.wav -i F:/upwork/whatsapp-bot/assets/audios/s2.wav -i F:/upwork/whatsapp-bot/assets/audios/prayer-part7.wav -i F:/upwork/whatsapp-bot/assets/audios/asservachanam-part1.wav -i F:/upwork/whatsapp-bot/assets/audios/asservachanam-part2.wav -i F:/upwork/whatsapp-bot/assets/audios/asservachanam-part3.wav -i F:/upwork/whatsapp-bot/assets/audios/conclusion.wav  -filter_complex [0:0][1:0][2:0][3:0][4:0][5:0][6:0]concat=n=7:v=0:a=1[out]  -map [out] F:/output.wav";
+
+let command1 = `ffmpeg -i F:/upwork/whatsapp-bot/assets/audios/intro.wav -i F:/upwork/whatsapp-bot/assets/audios/sankalpam-part2.wav -i \
+F:/upwork/whatsapp-bot/assets/audios/health_excellent.wav -i F:/upwork/whatsapp-bot/assets/audios/emotions_negative.wav -i F:/upwork/whatsapp-bot/assets/audios/profession_excellent.wav -i \
+F:/upwork/whatsapp-bot/assets/audios/luck_average.wav -i F:/upwork/whatsapp-bot/assets/audios/personal-life-average.wav -i F:/upwork/whatsapp-bot/assets/audios/travel-excellent.wav -i F:/upwork/whatsapp-bot/assets/audios/prayer-part7.wav -i F:/upwork/whatsapp-bot/assets/audios/asservachanam-part1.wav  -i F:/upwork/whatsapp-bot/assets/audios/asservachanam-part2.wav -i F:/upwork/whatsapp-bot/assets/audios/asservachanam-part3.wav -i F:/upwork/whatsapp-bot/assets/audios/conclusion.wav -filter_complex [0:0][1:0][2:0][3:0][4:0][5:0][6:0][7:0][8:0][9:0][10:0][11:0][12:0]concat=n=13:v=0:a=1[out]  -map [out] F:/output.wav`;
+
+const saveAudiofiles = async () => {
+  let users = await UserModel.find({});
+  // console.log(users);
+  for (let user of users) {
+    let data = await NakshatraModel.findOne({
+      birth_moon_nakshatra: user.birth_moon_nakshatra
+    });
+    let { sentiment } = data;
+    let command = `ffmpeg -i F:/upwork/whatsapp-bot/assets/audios/intro.wav -i F:/upwork/whatsapp-bot/assets/audios/sankalpam-part2.wav`;
+    switch (sentiment.health) {
+      case "excellent":
+        command += ` -i \t F:/upwork/whatsapp-bot/assets/audios/health_excellent.wav`;
+        break;
+      case "average":
+        command += ` -i \t F:/upwork/whatsapp-bot/assets/audios/health_average.wav`;
+        break;
+      case "negative":
+        command += ` -i \t F:/upwork/whatsapp-bot/assets/audios/health_negative.wav`;
+        break;
+      default:
+        break;
+    }
+    switch (sentiment.emotions) {
+      case "excellent":
+        command += ` -i F:/upwork/whatsapp-bot/assets/audios/emotions_excellent.wav`;
+        break;
+      case "average":
+        command += ` -i F:/upwork/whatsapp-bot/assets/audios/emotions_average.wav`;
+        break;
+      case "negative":
+        command += ` -i F:/upwork/whatsapp-bot/assets/audios/emotions_negative.wav`;
+        break;
+      default:
+        break;
+    }
+    switch (sentiment.profession) {
+      case "excellent":
+        command += ` -i F:/upwork/whatsapp-bot/assets/audios/profession_excellent.wav`;
+        break;
+      case "average":
+        command += ` -i F:/upwork/whatsapp-bot/assets/audios/profession_average.wav`;
+        break;
+      case "negative":
+        command += ` -i F:/upwork/whatsapp-bot/assets/audios/profession_negative.wav`;
+        break;
+      default:
+        break;
+    }
+    switch (sentiment.luck) {
+      case "excellent":
+        command += ` -i \t F:/upwork/whatsapp-bot/assets/audios/luck_excellent.wav`;
+        break;
+      case "average":
+        command += ` -i \t F:/upwork/whatsapp-bot/assets/audios/luck_average.wav`;
+        break;
+      case "negative":
+        command += ` -i \t F:/upwork/whatsapp-bot/assets/audios/luck_negative.wav`;
+        break;
+      default:
+        break;
+    }
+    switch (sentiment.personal_life) {
+      case "excellent":
+        command += ` -i F:/upwork/whatsapp-bot/assets/audios/personal-life-excellent.wav`;
+        break;
+      case "average":
+        command += ` -i F:/upwork/whatsapp-bot/assets/audios/personal-life-average.wav`;
+        break;
+      case "negative":
+        command += ` -i F:/upwork/whatsapp-bot/assets/audios/personal-life-negative.wav`;
+        break;
+      default:
+        break;
+    }
+    switch (sentiment.travel) {
+      case "excellent":
+        command += ` -i F:/upwork/whatsapp-bot/assets/audios/travel-excellent.wav`;
+        break;
+      case "average":
+        command += ` -i F:/upwork/whatsapp-bot/assets/audios/travel-average.wav`;
+        break;
+      case "negative":
+        command += ` -i F:/upwork/whatsapp-bot/assets/audios/travel-negative.wav`;
+        break;
+      default:
+        break;
+    }
+    command += ` -i F:/upwork/whatsapp-bot/assets/audios/prayer-part7.wav -i F:/upwork/whatsapp-bot/assets/audios/asservachanam-part1.wav  -i F:/upwork/whatsapp-bot/assets/audios/asservachanam-part2.wav -i F:/upwork/whatsapp-bot/assets/audios/asservachanam-part3.wav -i F:/upwork/whatsapp-bot/assets/audios/conclusion.wav -filter_complex [0:0][1:0][2:0][3:0][4:0][5:0][6:0][7:0][8:0][9:0][10:0][11:0][12:0]concat=n=13:v=0:a=1[out]  -map [out] F:/output.wav`;
+    // await combineAudio(command);
+    // console.log(command);
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+    });
+    return command1;
+  }
+};
+
+saveAudiofiles()
+  .then(console.log)
+  .catch(console.error);
